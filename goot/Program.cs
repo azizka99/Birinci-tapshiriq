d66a4f9dc98c85f96ddeba4b673442edf1b84ab9
Aziz <azizka.ibragimov@gmail.com>
2021-01-26 13:50:42 +0200

    Add project files.

---
+++ goot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace goot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nem = "Salam baki necesen?";
+
+            standEnd(nem);
+            
+        }
+
+        static string standEnd(string A)
+        {
+
+            string[] B = A.Split(' ');
+            string lastvariable = B[0] + B[(B.Length - 1)];
+
+            Console.WriteLine(lastvariable);
+            return "";
+        }
+    }
+}
